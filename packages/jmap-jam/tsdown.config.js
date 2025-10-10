bc93158fbe959c2b052b41bb5303535639518c66
Hunter Tunnicliff <hunter@tunnicliff.co>
2025-10-10 13:05:26 -0700

    Customize tsdown config

--- packages/jmap-jam/tsdown.config.js
+++ packages/jmap-jam/tsdown.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  outDir: "dist",
-  format: "esm",
-  clean: true,
-  minifyWhitespace: true,
-  minifySyntax: true,
-  target: ["es2018"],
-  sourcemap: true,
-  dts: true
+  target: ["esnext", "node18"],
+  platform: "neutral",
+  attw: {
+    profile: "esmOnly"
+  },
+  minify: true
 });
