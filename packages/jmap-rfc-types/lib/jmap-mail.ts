d9de5566cf1d51808901ce15cd3399c51c70c506
Hunter Tunnicliff <hunter@tunnicliff.co>
2026-09-11 14:58:27 -0700

    Fix formatting

--- packages/jmap-rfc-types/lib/jmap-mail.ts
+++ packages/jmap-rfc-types/lib/jmap-mail.ts
@@ -837,13 +837,13 @@ export type EmailBodyValue = {
    * This is true if malformed sections were found while decoding
    * the charset, the charset was unknown, or the content-transfer-
    * encoding was unknown.
-   * 
+   *
    * @default false
    */
   isEncodingProblem?: boolean;
   /**
    * This is true if the "value" has been truncated.
-   * 
+   *
    * @default false
    */
   isTruncated?: boolean;
